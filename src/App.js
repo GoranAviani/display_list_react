ca365d5f31a81a0e188fab616174695999812539
GoranAviani <goranaviani@gmail.com>
2022-05-14 12:48:49 +0200

    display_list_react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayList from "./DisplayList/DisplayList";
+
 
 const LIST = ["a", "b", "c"]
 
 function App() {
   return (
    <div>
-
+      <DisplayList/>
     </div>
   );
 }
